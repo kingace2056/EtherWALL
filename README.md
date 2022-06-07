c6fd8d74ccc5425ed738ad883fb323fdbe4985ca
sarthak <sarthak123parajuli@gmail.com>
2022-06-07 22:35:04 +0545

    getting started with EtherWALL

---
+++ README.md
@@ -0,0 +1,8 @@
+# EtherWALL
+
+A crypto wallet UI kit
+
+## First thing first
+
+UI inspired from https://whollet.io/
+Made with Flutter 3 ❤️
---
+++ android/app/src/main/kotlin/com/example/etherwall/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.etherwall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
