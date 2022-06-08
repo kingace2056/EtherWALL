b8d1d24a6629edcd1ae35754955c51f07f51d682
sarthak <sarthak123parajuli@gmail.com>
2022-06-08 22:43:46 +0545

    not much progress today

--- lib/constraints.dart
+++ lib/constraints.dart
@@ -1,2 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
 final String logoadr =
     'https://ethereum.org/static/a183661dd70e0e5c70689a0ec95ef0ba/cdbe4/eth-diamond-purple.webp';
+Color kPrimaryColor = Color(0xFF347AF0);
+Color kGreen = Color(0xFF75BF72);
+Color kRed = Color(0xFFDF5060);
+Color kYellow = Color(0xFFfdb32a);
+Color kMidNightBlue = Color(0xFF0d1f3c);
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:etherwall/screens/portfolio/portmain.dart';
 import 'package:flutter/material.dart';
 
 import '../constraints.dart';
@@ -13,51 +14,144 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: const Color.fromARGB(255, 224, 233, 248),
       body: Center(
           child: Padding(
-              padding: const EdgeInsets.fromLTRB(10, 80, 10, 10),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      logoadr,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Login to ',
-                      style: TextStyle(
-                        color: Color.fromARGB(146, 255, 255, 255),
-                        fontSize: 25,
-                      ),
-                    ),
-                    Text(
-                      'EtherWALL',
-                      style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.white),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.45),
-                    Form(
-                        child: Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            labelText: 'Email',
-                            fillColor: Colors.white,
-                            labelStyle: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
+              padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
+              child: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
                         ),
-                      ],
-                    ))
-                  ]))),
+                      ),
+                      SizedBox(height: 20),
+                      Image.network(
+                        logoadr,
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        '',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30)),
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 10, 8, 0),
+                          child: Form(
+                              child: Column(
+                            // ignore: prefer_const_literals_to_create_immutables
+                            children: [
+                              const TextField(
+                                decoration: InputDecoration(
+                                  isDense: true,
+                                  prefixIcon:
+                                      Icon(Icons.email, color: Colors.black),
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 10),
+                                  labelText: 'Email',
+                                  hintText: 'someone@example.com',
+                                  labelStyle: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 30,
+                              ),
+                              const TextField(
+                                obscureText: true,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                decoration: InputDecoration(
+                                  suffixIcon: Icon(Icons.remove_red_eye),
+                                  isDense: true,
+                                  prefixIcon:
+                                      Icon(Icons.email, color: Colors.black),
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 10),
+                                  labelText: 'Password',
+                                  hintText: 'Password',
+                                  labelStyle: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                alignment: Alignment.bottomRight,
+                                child: const Text('Forgot your password?',
+                                    style: TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal)),
+                              ),
+                              const SizedBox(height: 200),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => MainPort(),
+                                      ));
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(
+                                        MediaQuery.of(context).size.width * 0.4,
+                                        40),
+                                    primary: Colors.blue,
+                                    onPrimary: Colors.white),
+                                child: const Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Don\'t have an account?',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal),
+                                  ),
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => MainPort(),
+                                            ));
+                                      },
+                                      child: const Text('Sign Up')),
+                                ],
+                              ),
+                            ],
+                          )),
+                        ),
+                      )
+                    ]),
+              ))),
     );
   }
 }
